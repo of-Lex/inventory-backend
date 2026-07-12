8f20a1e6c6a83291a5d815c5056bfe5bf18601a7
of_Lex <of_lex@mail.ru>
2026-07-12 23:04:10 +1000

    Ulits: getDistance, Класс Position

---
+++ src/utils/index.ts
@@ -0,0 +1,9 @@
+import { Position } from "./Position.js";
+
+export class Utils {
+  private constructor() { }
+  
+  static getDistance(pos1: Position, pos2: Position): number {
+    return Math.sqrt((pos1.x - pos2.x) ** 2 + (pos1.y - pos2.y) ** 2);
+  }
+}
