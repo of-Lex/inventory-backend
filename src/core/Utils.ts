1101ed290d8f2df1773a1b90178c69273fa980cb
of_Lex <of_lex@mail.ru>
2026-07-13 19:41:27 +1000

    Utils: sleep, logWithTime

--- src/core/Utils.ts
+++ src/core/Utils.ts
@@ -6,4 +6,15 @@ export class Utils {
   static getDistance(pos1: Position, pos2: Position): number {
     return Math.sqrt((pos1.x - pos2.x) ** 2 + (pos1.y - pos2.y) ** 2);
   }
+
+  static async sleep(ms: number): Promise<boolean> {
+    await new Promise(resolve => setTimeout(resolve, ms));
+    return true;
+  }
+
+  static logWithTime(message: string): void {
+    const currentDate = new Date()
+    const time = currentDate.toLocaleTimeString();
+    console.log(`[${time}.${String(currentDate.getMilliseconds()).padStart(3, '0')}] ${message}`);
+  }
 }
